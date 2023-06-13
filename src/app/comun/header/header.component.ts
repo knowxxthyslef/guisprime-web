c9dfecb5d5eba8b961816dd248b4ca5df8d829c1
Brayan Hernandez <branhdzluz@gmail.com>
2023-06-12 20:27:21 -0600

    Header con logout

--- src/app/comun/header/header.component.ts
+++ src/app/comun/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+  }
+
+  cerrarSesion(){
+    this.accountService.logout();
   }
 
 }
--- src/app/comun/services/account.service.ts
+++ src/app/comun/services/account.service.ts
@@ -21,6 +21,8 @@ export class AccountService {
     
   }
 
-  
+  logout(){
+    this.router.navigate(['login']);
+  }
 
 }
