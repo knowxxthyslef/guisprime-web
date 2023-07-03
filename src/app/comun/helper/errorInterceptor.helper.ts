5fa96f3b1ce7c74a2a185d4fdc61de2f8561922a
Brayan Hernandez <branhdzluz@gmail.com>
2023-07-03 12:01:35 -0600

    se excluye token de error interceptor

--- src/app/comun/helper/errorInterceptor.helper.ts
+++ src/app/comun/helper/errorInterceptor.helper.ts
@@ -10,26 +10,42 @@ import { AccountService } from '../services/account.service';
 @Injectable()
 export class ErrorInterceptorHelper implements HttpInterceptor {
 
+    private excludeService: Array<string> = [
+        '/v1/oauth/token',
+    ];
+
     constructor(
         private router: Router,
         private accountService: AccountService,
     ){}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            const error = (err.error && err.error.mensaje) || (err.error && err.error.error_description) || err.name;
-            if(err.error && (err.error.error === 'unauthorized' ||  err.error.error === 'invalid_token') ){
+        if (this.isServiceExcluded(request.url) === false) {
+            return next.handle(request).pipe(catchError(err => {
+                const error = (err.error && err.error.mensaje) || (err.error && err.error.error_description) || err.name;
+                if(err.error && (err.error.error === 'unauthorized' ||  err.error.error === 'invalid_token') ){
+                    
+                    this.accountService.logout();
                 
-                this.accountService.logout();
-            
-            }else if(err.error && (err.name === 'HttpErrorResponse')){
-                return throwError(err.error);                
-            }
-            else{
-                
-            }
-            return throwError(error);
-        }))
+                }else if(err.error && (err.name === 'HttpErrorResponse')){
+                    return throwError(() => error);      
+                }
+                else{
+                    
+                }
+               return throwError(() => error);
+            }))
+        }else{
+            return next.handle(request);
+        }
     }
-    
+
+    private isServiceExcluded(url: string): boolean {
+        const found = this.excludeService.filter((service) => {
+            if (url.includes(service)) {
+                return service;
+            }
+        });
+        return found.length > 0;
+    }
 }
